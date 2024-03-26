e89d8af5e1e8bf21b0996b374e8bce38c6f9b2ef
kalolani <kgemechu@gmail.com>
2024-03-26 11:49:10 +0300

    fixed errors

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,16 +1,12 @@
 import PageNav from "../components/PageNav";
 
 function PageNotFound() {
-<<<<<<< HEAD
   return (
     <div>
       <PageNav />
       <h1>page not found :( </h1>
     </div>
   );
-=======
-  return <div>pagenotfound :( </div>;
->>>>>>> b456b9469deb93ac207a403d869d0a52a7f597ba
 }
 
 export default PageNotFound;
