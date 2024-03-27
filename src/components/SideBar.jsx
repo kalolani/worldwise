36d479a1a39beab9aad16e821571c646764fcbe9
kal_olani <123155716+kalolani@users.noreply.github.com>
2024-03-27 17:10:39 +0300

    bulid the app layout  page

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,18 @@
+import styles from "./SideBar.module.css";
+
+function SideBar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <p>List of cities</p>
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright{new Date().getFullYear()} by worldwise Inc.
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default SideBar;
--- src/pages/Applayout.jsx
+++ src/pages/Applayout.jsx
@@ -1,7 +1,13 @@
 import styles from "./AppLayout.module.css";
-
+import SideBar from "../components/SideBar";
+import Map from "../components/Map";
 function Applayout() {
-  return <div className={styles.app}>App</div>;
+  return (
+    <div className={styles.app}>
+      <SideBar />
+      <Map />
+    </div>
+  );
 }
 
 export default Applayout;
