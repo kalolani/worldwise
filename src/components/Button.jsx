0fcc1f82398896e6c653820efd6194dbf67323f4
kalolani <kgemechu@gmail.com>
2024-03-31 13:04:13 +0300

    added the form component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* eslint-disable react/prop-types */
 import styles from "./Button.module.css";
 function Button({ children, onClick, type }) {
@@ -10,13 +9,3 @@ function Button({ children, onClick, type }) {
 }
 
 export default Button;
-=======
-import styles from "./Button.module.css";
-function Button({ children, type, onClick }) {
-  return (
-    <button className={`${styles.btn} ${styles[type]}`}>{children}</button>
-  );
-}
-
-export default Button;
->>>>>>> 4d547c2c730b0007df06ccf149813537f49ca39e
